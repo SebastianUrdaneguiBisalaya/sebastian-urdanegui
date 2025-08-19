08569a146b3efebca927e3da6b72254f71737c9b
Sebastian Urdanegui <sebasurdanegui@gmail.com>
2025-08-19 13:58:12 -0500

    chore: install preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   i18n: {
     locales:["en", "es"],
     defaultLocale: "en",
     routing: {
       prefixDefaultLocale: false,
     }
-  }
+  },
+
+  integrations: [preact()]
 });
