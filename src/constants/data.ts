0ec3395b7c0f87aee221cac8e47c5dbb688198ab
Sebastian Urdanegui <sebasurdanegui@gmail.com>
2025-08-19 01:42:28 -0500

    chore: add dark mode toggle and i18n

---
+++ src/constants/data.ts
@@ -0,0 +1,32 @@
+export const headers = [
+    {
+        lang: "en",
+        content: "About",
+        url: "/"
+    },
+    {
+        lang: "es",
+        content: "Acerca de mí",
+        url: "/es/"
+    },
+    {
+        lang: "en",
+        content: "Projects",
+        url: "/projects"
+    },
+    {
+        lang: "es",
+        content: "Proyectos",
+        url: "/es/projects"
+    },
+    {
+        lang: "en",
+        content: "Blog",
+        url: "/blog"
+    },
+    {
+        lang: "es",
+        content: "Blog",
+        url: "/es/blog"
+    }
+]
