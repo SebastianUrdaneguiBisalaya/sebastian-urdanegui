76f8e42d7c7accb2cc57bc49adc9e441afa943ce
Sebastian Urdanegui <sebasurdanegui@gmail.com>
2025-08-21 18:43:23 -0500

    styles: hidden view data when the content type is different a blog

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -5,7 +5,7 @@ import { headersTable } from "@constants/data";
 interface Item {
     date: string;
     title: string;
-    views: number;
+    views?: number;
     comments?: number;
     entity?: string;
     url: string;
@@ -43,9 +43,13 @@ export default function Table ({ lang, type }: Props) {
                 <span class="grow font-sora text-dark/60 dark:text-white/60 text-xs sm:text-sm font-light text-left pb-2">
                     {headers[1].content}
                 </span>
-                <span class="w-[60px] font-reddit text-dark/60 dark:text-white/60 text-xs sm:text-sm font-light text-center pb-2">
-                    {headers[2].content}
-                </span>
+                {
+                    type === "blog" && (
+                        <span class="w-[60px] font-reddit text-dark/60 dark:text-white/60 text-xs sm:text-sm font-light text-center pb-2">
+                            {headers[2].content}
+                        </span>
+                    )
+                }
                 {
                     type === "blog" && (
                         <span class="w-[75px] font-reddit text-dark/60 dark:text-white/60 text-xs sm:text-sm font-light text-center pb-2">
@@ -61,7 +65,7 @@ export default function Table ({ lang, type }: Props) {
                         <a
                             href={item.url}
                             {...(isExternal ? { target: "_blank", rel: "noopener noreferrer" } : {})}
-                            class={`grid ${type === "blog" ? "grid-cols-[70px_minmax(0,1fr)_60px_75px]" : "grid-cols-[70px_minmax(0,1fr)_60px]"} gap-4 py-2 px-0 sm:px-2 w-full cursor-pointer border-t-[0.5px] border-gray-500 hover:bg-dark/5 dark:hover:bg-white/20 group`}
+                            class={`grid ${type === "blog" ? "grid-cols-[70px_minmax(0,1fr)_60px_75px]" : "grid-cols-[70px_minmax(0,1fr)]"} gap-4 py-2 px-0 sm:px-2 w-full cursor-pointer border-t-[0.5px] border-gray-500 hover:bg-dark/5 dark:hover:bg-white/20 group`}
                         >
                             <div class="flex flex-col">
                                 <span class="font-reddit text-dark/80 dark:text-white/80 text-sm py-2">{extractYearFromDate(item.date)}</span>
@@ -72,14 +76,18 @@ export default function Table ({ lang, type }: Props) {
                                     item.entity && <span class="font-reddit text-dark/60 dark:text-white/75 text-xs font-light">{item.entity}</span>
                                 }
                             </div>
-                            <div class="flex flex-row items-center justify-center gap-2">
-                                <span class="opacity-50 text-dark/80 dark:text-white/80">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 32 32"><path fill="currentColor" d="M30.94 15.66A16.69 16.69 0 0 0 16 5A16.69 16.69 0 0 0 1.06 15.66a1 1 0 0 0 0 .68A16.69 16.69 0 0 0 16 27a16.69 16.69 0 0 0 14.94-10.66a1 1 0 0 0 0-.68M16 25c-5.3 0-10.9-3.93-12.93-9C5.1 10.93 10.7 7 16 7s10.9 3.93 12.93 9C26.9 21.07 21.3 25 16 25"/><path fill="currentColor" d="M16 10a6 6 0 1 0 6 6a6 6 0 0 0-6-6m0 10a4 4 0 1 1 4-4a4 4 0 0 1-4 4"/></svg>
-                                </span>
-                                <span class="font-reddit text-dark/80 dark:text-white/80 text-sm">
-                                    {item.views ?? 0}
-                                </span>
-                            </div>
+                            {
+                                type === "blog" && (
+                                    <div class="flex flex-row items-center justify-center gap-2">
+                                        <span class="opacity-50 text-dark/80 dark:text-white/80">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 32 32"><path fill="currentColor" d="M30.94 15.66A16.69 16.69 0 0 0 16 5A16.69 16.69 0 0 0 1.06 15.66a1 1 0 0 0 0 .68A16.69 16.69 0 0 0 16 27a16.69 16.69 0 0 0 14.94-10.66a1 1 0 0 0 0-.68M16 25c-5.3 0-10.9-3.93-12.93-9C5.1 10.93 10.7 7 16 7s10.9 3.93 12.93 9C26.9 21.07 21.3 25 16 25"/><path fill="currentColor" d="M16 10a6 6 0 1 0 6 6a6 6 0 0 0-6-6m0 10a4 4 0 1 1 4-4a4 4 0 0 1-4 4"/></svg>
+                                        </span>
+                                        <span class="font-reddit text-dark/80 dark:text-white/80 text-sm">
+                                            {item.views ?? 0}
+                                        </span>
+                                    </div>
+                                )
+                            }
                             {
                                 type === "blog" && (
                                     <div class="flex flex-row items-center justify-center gap-2">
