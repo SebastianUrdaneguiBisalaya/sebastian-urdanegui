467da26da8852bf00ca1cffd8c98bf719f0a98f2
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2025-09-24 15:19:46 -0500

    hidden elements when the width of page is less than 'sm'

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -46,14 +46,14 @@ export default function Table ({ lang, type }: Props) {
                 </span>
                 {
                     type === "blog" && (
-                        <span class="w-[70px] font-sora prose dark:prose-invert text-xs sm:text-sm font-light text-center pb-2">
+                        <span class="hidden sm:block w-[70px] font-sora prose dark:prose-invert text-xs sm:text-sm font-light text-center pb-2">
                             {headers[2].content}
                         </span>
                     )
                 }
                 {
                     type === "blog" && (
-                        <span class="w-[95px] font-sora prose dark:prose-invert text-xs sm:text-sm font-light text-center pb-2">
+                        <span class="hidden sm:block w-[95px] font-sora prose dark:prose-invert text-xs sm:text-sm font-light text-center pb-2">
                             {headers[3].content}
                         </span>
                     )
@@ -67,7 +67,7 @@ export default function Table ({ lang, type }: Props) {
                         <a
                             href={url}
                             {...(isExternal ? { target: "_blank", rel: "noopener noreferrer" } : {})}
-                            class={`grid ${type === "blog" ? "grid-cols-[70px_minmax(0,1fr)_70px_95px]" : "grid-cols-[70px_minmax(0,1fr)]"} gap-4 py-2 px-0 sm:px-2 w-full cursor-pointer border-t-[0.5px] border-gray-500 hover:bg-dark/5 dark:hover:bg-white/20 group`}
+                            class={`grid ${type === "blog" ? "grid-cols-[70px_minmax(0,1fr)] sm:grid-cols-[70px_minmax(0,1fr)_70px_95px]" : "grid-cols-[70px_minmax(0,1fr)]"} gap-4 py-2 px-0 sm:px-2 w-full cursor-pointer border-t-[0.5px] border-gray-500 hover:bg-dark/5 dark:hover:bg-white/20 group`}
                         >
                             <div class="flex flex-col">
                                 <span class="font-reddit prose dark:prose-invert text-sm py-2">{extractYearFromDate(item.date)}</span>
@@ -81,7 +81,7 @@ export default function Table ({ lang, type }: Props) {
                             </div>
                             {
                                 type === "blog" && (
-                                    <div class="flex flex-row items-center justify-center gap-2">
+                                    <div class="hidden sm:flex flex-row items-center justify-center gap-2">
                                         <span class="opacity-50 prose dark:prose-invert">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 32 32"><path fill="currentColor" d="M30.94 15.66A16.69 16.69 0 0 0 16 5A16.69 16.69 0 0 0 1.06 15.66a1 1 0 0 0 0 .68A16.69 16.69 0 0 0 16 27a16.69 16.69 0 0 0 14.94-10.66a1 1 0 0 0 0-.68M16 25c-5.3 0-10.9-3.93-12.93-9C5.1 10.93 10.7 7 16 7s10.9 3.93 12.93 9C26.9 21.07 21.3 25 16 25"/><path fill="currentColor" d="M16 10a6 6 0 1 0 6 6a6 6 0 0 0-6-6m0 10a4 4 0 1 1 4-4a4 4 0 0 1-4 4"/></svg>
                                         </span>
@@ -93,7 +93,7 @@ export default function Table ({ lang, type }: Props) {
                             }
                             {
                                 type === "blog" && (
-                                    <div class="flex flex-row items-center justify-center gap-2">
+                                    <div class="hidden sm:flex flex-row items-center justify-center gap-2">
                                         <span class="opacity-50 prose dark:prose-invert">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.3 16.7a9 9 0 1 1 3 3L3 21z"/></svg>
                                         </span>
